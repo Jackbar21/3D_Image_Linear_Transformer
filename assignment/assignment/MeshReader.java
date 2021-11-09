c54ee77156820b1ba61037196d37f186c3058a6e
Jackbar21 <86801013+Jackbar21@users.noreply.github.com>
2021-11-09 12:45:38 -0500

    Delete assignment directory

--- assignment/assignment/MeshReader.java
+++
@@ -1,8 +0,0 @@
-package assignment;
-
-import java.io.FileNotFoundException;
-import java.util.HashSet;
-
-public interface MeshReader {
-	public HashSet<Polygon> read(String fileName) throws FileNotFoundException;
-}
--- assignment/assignment/MeshWriter.java
+++
@@ -1,9 +0,0 @@
-package assignment;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.HashSet;
-
-public interface MeshWriter {
-	public void write(String fileName, HashSet<Polygon> polygons) throws FileNotFoundException, IOException;
-}
